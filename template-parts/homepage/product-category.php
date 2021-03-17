207ddda2667bcbf5d6cd2750d626ac52cd51852c
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-03-17 14:44:55 +0600

    home page separation

---
+++ template-parts/homepage/product-category.php
@@ -0,0 +1,17 @@
+<!--shop category start-->
+<section class="space-3">
+        <div class="container sm-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="title"> Shop By Category</h2>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <?php echo do_shortcode("[product_categories columns=5]"); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--shop category end-->
