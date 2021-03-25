7ba113f5f18a78082551b47ccdc1b6e02186966f
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-03-25 13:51:00 +0600

    woocommerce loop/add to cart overwright and change some function and also mark up

--- functions.php
+++ functions.php
@@ -89,3 +89,19 @@ function simpleshop_subcategory_count_html($markup){
 	return $markup;
 }
 add_filter('woocommerce_subcategory_count_html','simpleshop_subcategory_count_html');
+
+add_filter('wp_calculate_image_sizes','__return_empty_array');
+add_filter('wp_calculate_image_srcset','__return_empty_array');
+
+remove_action('woocommerce_before_shop_loop_item_title','woocommerce_show_product_loop_sale_flash','10');
+remove_filter('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title','10');
+remove_filter('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_rating','5');
+remove_filter('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_price','10');
+remove_filter('woocommerce_after_shop_loop_item','woocommerce_template_loop_add_to_cart','10');
+add_action('woocommerce_before_shop_loop_item_title','woocommerce_template_loop_add_to_cart',10);
+
+function simpleshop_product_add_to_cart_text($text)
+{
+	return '<i class="fa fa-shoping-basket"></i>';
+}
+add_filter('woocommerce_product_add_to_cart_text','simpleshop_product_add_to_cart_text');
