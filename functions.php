03cb6aae9c932e8d35d6f9a7282d43511f6e7635
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-03-25 17:06:56 +0600

    end of mark up modify

--- functions.php
+++ functions.php
@@ -105,3 +105,25 @@ function simpleshop_product_add_to_cart_text($text)
 	return '<i class="fa fa-shoping-basket"></i>';
 }
 add_filter('woocommerce_product_add_to_cart_text','simpleshop_product_add_to_cart_text');
+
+function simpleshop_before_shop_loop_item()
+{
+	echo '<div class="product-wrap">';
+}
+add_action('woocommerce_before_shop_loop_item','simpleshop_before_shop_loop_item');
+
+function simpleshop_before_shop_loop_item_title()
+{
+	echo '</div><div class="woocommerce-product-title-wrap">';
+}
+add_action('woocommerce_before_shop_loop_item_title','simpleshop_before_shop_loop_item_title');
+
+add_action('woocommerce_after_shop_loop_item_title','woocommerce_show_product_loop_sale_flash',11);
+add_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_price');
+
+function simpleshop_after_shop_loop_item_title()
+{
+	echo '<a href="#" class="wish-list"><i class="fa fa-heart-o"></i></a></div>';
+}
+add_action('woocommerce_after_shop_loop_item_title','simpleshop_after_shop_loop_item_title');
+
